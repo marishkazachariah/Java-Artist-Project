4c277ad6363a1ba660692563b00fd8e79135e886
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-05 17:24:23 +0200

    WIP on implementing search method using binary search

--- ArtistsImplementation.java
+++ ArtistsImplementation.java
@@ -15,19 +15,51 @@ public class ArtistsImplementation implements Artists, Searchable{
         return artists.get(index);
     }
 
+    @Override
+    public void editArtist(String oldName, String newName) {
+        for (Artist artist : artists) {
+            if (artist.getName().equalsIgnoreCase(oldName)) {
+                artist.setName(newName);
+            }
+        }
+    }
+
     // search
+//    @Override
+//    public List<Artist> search(String lastname) {
+//        List<Artist> matchingArtists = new ArrayList<>();
+//
+//        for (Artist artist : artists) {
+//            String artistName = artist.getName().toLowerCase();
+//
+//            if (artistName.contains(lastname.toLowerCase())) {
+//                matchingArtists.add(artist);
+//            }
+//        }
+//
+//        return matchingArtists;
+//    }
+
+    // Implementing the search method using a binary search
     @Override
     public List<Artist> search(String lastname) {
         List<Artist> matchingArtists = new ArrayList<>();
+        int left = 0;
+        int right = artists.size() - 1;
 
-        for (Artist artist : artists) {
-            String artistName = artist.getName().toLowerCase();
+        while(left <= right) {
+            int mid = artists.size() / 2;
+            String midName = artists.get(mid).getName();
+            int midValue = midName.compareTo(lastname);
 
-            if (artistName.contains(lastname.toLowerCase())) {
-                matchingArtists.add(artist);
+            if(midValue == 0) {
+                matchingArtists.add(artists.get(midValue));
+            } else if (midValue < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-
         return matchingArtists;
     }
 }
