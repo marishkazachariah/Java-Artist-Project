91c7dc480cecfd601817fa71246d3f075635cef4
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-06 11:26:51 +0200

    remove search using binary search method

--- ArtistsImplementation.java
+++ ArtistsImplementation.java
@@ -25,41 +25,18 @@ public class ArtistsImplementation implements Artists, Searchable{
     }
 
     // search
-//    @Override
-//    public List<Artist> search(String lastname) {
-//        List<Artist> matchingArtists = new ArrayList<>();
-//
-//        for (Artist artist : artists) {
-//            String artistName = artist.getName().toLowerCase();
-//
-//            if (artistName.contains(lastname.toLowerCase())) {
-//                matchingArtists.add(artist);
-//            }
-//        }
-//
-//        return matchingArtists;
-//    }
-
-    // Implementing the search method using a binary search
     @Override
     public List<Artist> search(String lastname) {
         List<Artist> matchingArtists = new ArrayList<>();
-        int left = 0;
-        int right = artists.size() - 1;
 
-        while(left <= right) {
-            int mid = artists.size() / 2;
-            String midName = artists.get(mid).getName();
-            int midValue = midName.compareTo(lastname);
+        for (Artist artist : artists) {
+            String artistName = artist.getName().toLowerCase();
 
-            if(midValue == 0) {
-                matchingArtists.add(artists.get(midValue));
-            } else if (midValue < 0) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            if (artistName.contains(lastname.toLowerCase())) {
+                matchingArtists.add(artist);
             }
         }
+
         return matchingArtists;
     }
 }
