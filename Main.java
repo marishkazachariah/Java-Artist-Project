056dd0cee05773dac668a68e4fd1e93b6f36918b
marishkazachariah <marishka.zachariah@gmail.com>
2023-10-06 12:06:19 +0200

    test method in Main class

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        ArtistsImplementation artistsImplementation = new ArtistsImplementation();
+//        06.10.2023 Artist Database
+        ArtistDatabase artistDatabase = new ArtistDatabase();
+        artistDatabase.initializeDatabase();
 
-        artistsImplementation.add(new ArtistImplementation("Samson Adder"));
-        artistsImplementation.add(new ArtistImplementation("Jane Smith"));
-        artistsImplementation.add(new ArtistImplementation("Alice Johnson"));
-
-        List<Artist> matchingArtists = artistsImplementation.search("Adder");
-
-        for (Artist artist : matchingArtists) {
-            System.out.println("Artist: " + artist.getName());
-        }
+//        05.10.2023 - Search Function
+//        ArtistsImplementation artistsImplementation = new ArtistsImplementation();
+//
+//        artistsImplementation.add(new ArtistImplementation("Samson Adder"));
+//        artistsImplementation.add(new ArtistImplementation("Jane Smith"));
+//        artistsImplementation.add(new ArtistImplementation("Alice Johnson"));
+//
+//        List<Artist> matchingArtists = artistsImplementation.search("Adder");
+//
+//        for (Artist artist : matchingArtists) {
+//            System.out.println("Artist: " + artist.getName());
+//        }
     }
 }
